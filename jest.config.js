904acdc9d8e194ddd26eaad4433093974c408be4
parvin-noori <parvinnoori0@gmail.com>
2025-06-04 13:31:32 +0330

    fix bug in config test

--- jest.config.js
+++ jest.config.js
@@ -8,10 +8,9 @@ const createJestConfig = nextJest({
  
 // Add any custom config to be passed to Jest
 const config = {
-  // coverageProvider: 'v8',
-  // testEnvironment: 'jsdom',
   // Add more setup options before each test is run
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
   preset: 'ts-jest',
 }
  
