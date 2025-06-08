6fe0637de5969b79a9afce012bc7fe02caf2e870
parvin-noori <parvinnoori.dev@gmail.com>
2025-06-08 10:40:28 +0330

    update button.test

--- src/app/_components/button/button.test.tsx
+++ src/app/_components/button/button.test.tsx
@@ -1,7 +1,23 @@
 import { describe } from "node:test";
+import {render,screen} from "@testing-library/react"
+import { Button } from "./button";
 
 describe('Button', () => {
-    it('testing button', () => {
-        
+    test('renders a default button', () => {
+        const {getByText}=render(<Button>Click here</Button>);
+        expect(getByText('Click here')).toBeInTheDocument()
+    })
+
+    test('disables the button when disabled prop is true',()=>{
+        render(<Button disabled={true}>click here</Button>)
+        expect(screen.getByRole('button')).toBeDisabled()
+    })
+
+    test ('applied the correct css class for diffrent button variants',()=>{
+        const {rerender}=render(<Button variant="primary">click here</Button>)
+        expect(screen.getByRole('button')).toHaveClass('btn-primary');
+
+        rerender(<Button variant="info">click here</Button>);
+          expect(screen.getByRole('button')).toHaveClass('btn-info');
     })
 })
