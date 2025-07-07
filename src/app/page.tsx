9cdb758c26a218251f8375a2d15d12b010ba5b89
parvin-noori <parvinnoori0@gmail.com>
2025-07-07 11:49:40 +0330

    fetch newest blog posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,22 @@
 import Image from "next/image";
 import Colors from "./_components/colors/colors";
 import { Button } from "./_components/button";
+import { HomeHeroSection } from "./_components/home-hero-section/home-hero-section";
+import { CoursesSummary } from "./types/course-summary.interface";
 
-export default function Home() {
+ async function getNewestCourses(count:number):Promise<CoursesSummary[]>{
+ const res=await fetch (`https://api.classbon.com/api/courses/newest/${count}`)
+ return res.json()
+}
+
+export default async function Home() {
+  const newestCourses=await getNewestCourses(4)
   return (
     <>
-     <section className="bg-hero-pattern bg-no-repeat bg-center xl:bg-left mt-5 xl:mt-20">
-        <div className="container flex flex-col-reverse items-center xl:flex-row">
-          <div className="flex-1 flex flex-col gap-5 mt-12 pb-5 text-center xl:text-right">
-            <h3 className="text-xl xl:text-2xl dark:text-info">
-              خوش اومدی به ...
-            </h3>
-            <h1 className=" text-3xl lg:text-5xl xl:text-5xl font-black gradient">
-              مسیر صعود به قله‌های برنامه‌نویسی
-            </h1>
-            <p className="max-w-2xl  text-lg md:text-xl font-bold leading-8">
-              هر جای مسیرِ برنامه‌نویسی که باشی، با هم‌راهی استادهای باتجربهٔ
-              کلاسبن می‌تونی بدون محدودیت به قله‌های بالاتر صعود کنی. ما همیشه
-              هواتو داریم.
-            </p>
-            <div className="mt-5 flex gap-4">
-              <Button variant="primary" size="large">دوره های ری اکت و نکست</Button>
-              <Button variant="neutral" size="large">مشاوره برنامه نویسی</Button>
-            </div>
-            <Image
-              src="/images/frameworks.png"
-              className="grayscale mt-4 opacity-70 m-auto xl:m-0"
-              width={412}
-              height={39}
-              alt=""
-            />
-          </div>
-          <Image
-            src="/images/programmer-landing.svg"
-            alt=""
-            width={702}
-            height={521}
-          />
-        </div>
-      </section>
-     
+   <HomeHeroSection/>
+   {newestCourses.map(course=>(
+    <p key={course.id}>{course.title}</p>
+   ))}
     </>
   );
 }
---
+++ src/enums/course-level.enum.ts
@@ -0,0 +1,5 @@
+export enum CourseLevel{
+    beginners=0,
+    intermediate,
+    advance
+}
