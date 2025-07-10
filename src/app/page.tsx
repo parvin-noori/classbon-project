6b52386afdd1376f63aff4352726ccaee1cc1536
parvin-noori <parvinnoori0@gmail.com>
2025-07-10 12:20:12 +0330

    fetch blog posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,27 @@ import { CoursesSummary } from "./types/course-summary.interface";
 import { CourseCardList } from "./(courses)/_components/course-card-list";
 import { homeFeatures } from "@/data/home-features";
 import { Feature } from "./_components/feature/feature";
+import { IconArrowLeftFill } from "./_components/icons";
+import { BlogPostSummary } from "./types/blog-post-summary.interface";
 
  async function getNewestCourses(count:number):Promise<CoursesSummary[]>{
  const res=await fetch (`https://api.classbon.com/api/courses/newest/${count}`)
  return res.json()
 }
 
+ async function getNewestPosts(count:number):Promise<BlogPostSummary[]>{
+ const res=await fetch (`https://api.classbon.com/api/blog/newest/${count}`)
+ return res.json()
+}
+
 export default async function Home() {
-  const newestCourses=await getNewestCourses(4)
+  const newestCoursesData= getNewestCourses(4)
+  const newestPostsData= getNewestCourses(4)
+
+  const [newestCourses,newestPosts]=await Promise.all([newestCoursesData,newestPostsData])
+
+  console.log(newestPosts)
+
   return (
     <>
    <HomeHeroSection/>
@@ -34,6 +47,45 @@ export default async function Home() {
     </div>
     <CourseCardList courses={newestCourses}/>
    </section>
+   <section className="px-2 my-40">
+                {/* <div className="sticky top-0 pt-0 text-center"> */}
+                <div className="relative pt-0 text-center">
+                    <div className="bg-primary pointer-events-none absolute left-1/2 aspect-square w-1/2 -translate-x-1/2 -top-96 rounded-full opacity-10 blur-3xl"></div>
+
+                    <h2
+                        lang="en"
+                        className="gradient-reverse leading-[1.3] relative z-10 mx-auto inline-block text-[clamp(2rem,6vw,5.5rem)] font-black"
+                    >
+                        ReactJs & Next.js
+                    </h2>
+                    <p className="text-base-content/70  relative z-[2] py-4 m-auto md:text-3xl max-w-5xl font-light !leading-[1.7]">
+                        ری‌اکت و نکست‌جی‌اس برترین کتابخونه‌های فرانت‌اند و
+                        یکه‌تاز دنیای وب! پیشرفته‌ترین مباحث رو اینجا می تونی
+                        پیدا کنی. پس همین الان یادگیری رو شروع کن ما هم از
+                        ابتدای مسیر با آموزش‌های تخصصی و کاملاً کاربردی کنارت
+                        هستیم.
+                    </p>
+                    <div className="flex gap-3 justify-center">
+                        <Button
+                            variant="primary"
+                            size="large"
+                            className="mt-7"
+                            animatedIcon={true}
+                        >
+                            دوره‌های ری اکت و نکست‌ جی‌اس
+                            <IconArrowLeftFill fill="currentColor" />
+                        </Button>
+                        <Button
+                            variant="neutral"
+                            size="large"
+                            className="mt-7"
+                            animatedIcon={true}
+                        >
+                            مقالات ری اکت و نکست‌ جی‌اس
+                        </Button>
+                    </div>
+                </div>
+            </section>
     </>
   );
 }
