eb6e265b5d6a87acaf7662c967c46d4d2c0dcd6e
parvin-noori <parvinnoori0@gmail.com>
2025-07-22 12:33:46 +0330

    feat text placeholder

--- src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
@@ -3,12 +3,13 @@
 import { useParams } from "next/navigation"
 import { useCourseComments } from "../../_api/get-comments"
 import { Comment } from "@/app/_components/comment"
+import { TextPlaceholder } from "@/app/_components/placeholders"
 
 const CourseComments=()=>{
 
 const {slug}=useParams()
 
-const {data:comments}=useCourseComments({
+const {data:comments,isLoading}=useCourseComments({
     params:{
         slug:slug as string,
         page:1
@@ -19,6 +20,7 @@ const {data:comments}=useCourseComments({
         {comments?.data.map((comment)=>(
            <Comment key={`comment-${comment.id}`} variant="info" {...comment}/>
         ))}
+        {isLoading && <TextPlaceholder/>}
         </>
     )
 }
