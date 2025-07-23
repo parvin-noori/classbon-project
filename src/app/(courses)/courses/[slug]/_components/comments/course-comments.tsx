2d963f44d83ddb737256b71ae7338ed45a12c61b
parvin-noori <parvinnoori0@gmail.com>
2025-07-23 10:13:14 +0330

    [feat] infinite scroll

--- src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
@@ -4,23 +4,47 @@ import { useParams } from "next/navigation"
 import { useCourseComments } from "../../_api/get-comments"
 import { Comment } from "@/app/_components/comment"
 import { TextPlaceholder } from "@/app/_components/placeholders"
+import { Fragment, useEffect } from "react"
+import { useInView } from "react-intersection-observer"
+
 
 const CourseComments=()=>{
-
+    
+const {ref,inView}=useInView({})
 const {slug}=useParams()
 
-const {data:comments,isLoading}=useCourseComments({
+const {data:comments,error,hasNextPage,fetchNextPage,isFetchingNextPage,refetch}=useCourseComments({
     params:{
         slug:slug as string,
         page:1
     }
 })
+
+
+useEffect(()=>{
+    if(inView && hasNextPage){
+        fetchNextPage()
+    }
+},[inView,hasNextPage,fetchNextPage])
     return(
         <>
-        {comments?.data.map((comment)=>(
+        {comments?.pages.map((currentPage)=>(
+            <Fragment key={`course-comment-${currentPage}`}>
+                {currentPage.data.map((comment)=>(
+
            <Comment key={`comment-${comment.id}`} variant="info" {...comment}/>
+                ))}
+            </Fragment>
         ))}
-        {isLoading && <TextPlaceholder/>}
+
+       
+
+        {hasNextPage || isFetchingNextPage && (
+ <div ref={ref}>
+ <TextPlaceholder/>
+</div>
+        )}
+    
         </>
     )
 }
