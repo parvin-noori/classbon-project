afe1e83c58003f8ab9289c618ed24ab1148d7fd1
parvin-noori <parvinnoori0@gmail.com>
2025-07-28 14:02:26 +0330

    react hook form

--- src/app/(auth)/signin/components/signin-form.tsx
+++ src/app/(auth)/signin/components/signin-form.tsx
@@ -1,13 +1,23 @@
+'use client'
+
 import { Button } from "@/app/_components/button"
 import { TextBox } from "@/app/_components/textbox"
+import { useForm } from "react-hook-form"
+import { Signin } from "../types/signin.types"
 
 export const SigninForm = () => {
+
+  const {handleSubmit,register,formState:{errors}}=useForm<Signin>()
+
+  const onSubmit=(data:any)=>{
+    console.log(data)
+  }
     return(
         <>
         <h5 className="text-2xl">ورود | ثبت نام</h5>
         <p className="mt-2">دنیای شگفت انگیز برنامه نویسی در انتظار شماست!</p>
-        <form className="flex flex-col gap-6 mt-16">
-          <TextBox placeholder="شماره موبایل"/>
+        <form className="flex flex-col gap-6 mt-16" onSubmit={handleSubmit(onSubmit)}>
+          <TextBox {...register('mobile',{required:"شماره موبایل الزامی است"})} placeholder="شماره موبایل"/>
           <Button type="submit" variant="primary">
             تایید و دریافت کد
           </Button>
---
+++ src/app/(auth)/signin/types/signin.types.ts
@@ -0,0 +1,3 @@
+export interface Signin{
+    mobile:string
+}
