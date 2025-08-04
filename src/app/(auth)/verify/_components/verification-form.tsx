df39c32c4ff541e5071cba4a83feb3f85357cd2f
parvin-noori <parvinnoori0@gmail.com>
2025-08-04 13:01:42 +0330

    timer component

--- src/app/(auth)/verify/_components/verification-form.tsx
+++ src/app/(auth)/verify/_components/verification-form.tsx
@@ -3,8 +3,15 @@
 import { AuthCode } from "@/app/_components/auth-code/auth-code";
 import { AuthCodeRef } from "@/app/_components/auth-code/auth-code.types";
 import { Button } from "@/app/_components/button";
+import { Timer } from "@/app/_components/timer/timer";
 import Link from "next/link";
 import { useRef } from "react";
+
+const getTwoMinutesFromNow = () => {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 120);
+  return time
+};
 
 export const VerificationForm = () => {
   const authCodeRef = useRef<AuthCodeRef>(null);
@@ -14,14 +21,16 @@ export const VerificationForm = () => {
       <p className="mt-2">دنیای شگفت انگیز برنامه نویسی در انتظار شماست!</p>
       <form className="flex flex-col gap-6 mt-10 flex-1">
         <AuthCode
-        ref={authCodeRef}
+          ref={authCodeRef}
           onChange={(value) => {
             console.log(value);
           }}
           className="mt-10"
         />
-        Timer
-        <Button isLink={true} onClick={authCodeRef.current?.clear}>ارسال مجدد کد تایید</Button>
+        <Timer expiryTimestamp={getTwoMinutesFromNow()}/>
+        <Button isLink={true} onClick={authCodeRef.current?.clear}>
+          ارسال مجدد کد تایید
+        </Button>
         <Button type="submit" variant="primary">
           تایید و ادامه
         </Button>
---
+++ src/app/_components/timer/timer.tsx
@@ -0,0 +1,129 @@
+import { forwardRef, useImperativeHandle } from "react";
+import { Size } from "../types/size.type";
+import { TimerProps, TimerRef } from "./timer.types";
+import { useTimer } from "react-timer-hook";
+import classNames from "classnames";
+
+const sizeClasses: Record<Size, string> = {
+  tiny: "timer-xs",
+  small: "timer-sm",
+  normal: "timer-md",
+  large: "timer-lg",
+};
+
+const calculateTotalSeconds = (
+  days: number,
+  hours: number,
+  minutes: number,
+  seconds: number
+): number => days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds;
+
+export const Timer = forwardRef<TimerRef, TimerProps>(
+  (
+    {
+      expiryTimestamp,
+      autoStart,
+      onExpire,
+      size = "normal",
+      className,
+      showTitle = true,
+      showDays = true,
+      showHours = true,
+      variant = "primary",
+    },
+    ref
+  ) => {
+    const { days, hours, minutes, seconds, start, pause, restart, resume } =
+      useTimer({
+        expiryTimestamp,
+        autoStart,
+        onExpire,
+      });
+
+    const classes = classNames(
+      "timer",
+      { [`${sizeClasses[size]}`]: size },
+      { [`timer-${variant}`]: variant },
+      className
+    );
+
+    useImperativeHandle(ref, () => ({
+      start,
+      pause,
+      restart,
+      resume,
+    }));
+
+    const maxDaysValue = calculateTotalSeconds(30, 0, 0, 0);
+    const maxHourseValue = calculateTotalSeconds(0, 24, 0, 0);
+    const maxMinutesValue = calculateTotalSeconds(0, 0, 60, 0);
+    const maxSecondsValue = calculateTotalSeconds(0, 0, 0, 60);
+
+    const daysValue = calculateTotalSeconds(days, 0, 0, 0);
+    const hoursValue = calculateTotalSeconds(0, hours, 0, 0);
+    const minutesValue = calculateTotalSeconds(0, 0, minutes, 0);
+    const secondsValue = calculateTotalSeconds(0, 0, 0, seconds);
+
+    const renderTimerProgress = (
+      unit: number,
+      value: number,
+      maxValue: number,
+      datePart: string
+    ) => {
+      if (value !== null) {
+        return (
+          // <TimerProgress
+          //     value={value}
+          //     maxValue={maxValue}
+          //     datePart={datePart}
+          //     size={size}
+          //     showTitle={showTitle}
+          //     variant={variant}
+          // >
+          //     {unit}
+          // </TimerProgress>
+          <span>{unit}</span>
+        );
+      }
+    };
+
+    const timeUnits = [
+      {
+        show: seconds != null,
+        unit: seconds,
+        value: secondsValue,
+        maxValue: maxSecondsValue,
+        datePart: "seconds",
+      },
+      {
+        show: minutes != null,
+        unit: minutes,
+        value: minutesValue,
+        maxValue: maxMinutesValue,
+        datePart: "minutes",
+      },
+      {
+        show: showHours && hours != null,
+        unit: hours,
+        value: hoursValue,
+        maxValue: maxHourseValue,
+        datePart: "hours",
+      },
+      {
+        show: showDays && days != null,
+        unit: days,
+        value: daysValue,
+        maxValue: maxDaysValue,
+        datePart: "days",
+      },
+    ];
+
+    return (
+      <div className={`${classes} flex flex-row gap-4`} lang="en">
+        {timeUnits.map(({ show, unit, value, maxValue, datePart }) =>
+          show ? renderTimerProgress(unit, value, maxValue, datePart) : null
+        )}
+      </div>
+    );
+  }
+);
